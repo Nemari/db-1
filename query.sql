fe15e386b1639354e6273c20411c616f31e159e9
Mariia Hrytsaienko <44789399+Nemari@users.noreply.github.com>
2020-04-23 14:11:25 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,6 @@
 SELECT variety, AVG(grade) FROM rate GROUP BY variety;
 
-SELECT DISTINCT (select count(province) from manufacture
-where province='California')/(select count(province) from manufacture)*100 as per_cent FROM manufacture; 
+select count(province)/(select count(province)from manufacture)*100 as Procent, province from manufacture group by province 
 
 SELECT winery, grade FROM wine
 JOIN rate ON wine.wine_id=rate.wine_id;
