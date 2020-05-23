9fd40611c9e5c594830bb7a29c91d29997f49f21
Mariia Hrytsaienko <44789399+Nemari@users.noreply.github.com>
2020-05-23 12:45:53 +0300

    Update main.py

--- laba3/main.py
+++ laba3/main.py
@@ -1,40 +1,54 @@
+import chart_studio
+chart_studio.tools.set_credentials_file('Nemari', '4IXvHdK7WIJhR4iQor2V')
+import plotly.graph_objects as go
+
 import cx_Oracle
+import chart_studio.plotly as py
 
-username = 'system'
-password = 'Nemari123'
-databaseName = 'localhost/orcl.168.1.8'
-
-connection = cx_Oracle.connect(username, password, databaseName)
-
-cursor = connection.cursor()
-
-
-
+connection = cx_Oracle.connect('system', 'Nemari123', 'localhost/orcl.168.1.8')
+print("\nFirst query: average grade for each wine variety.")
 query1 = """SELECT WineType.wine_type as wine_type, AVG(Score.score)
 from Score
 join WineType on Score.wine_id = WineType.wine_id
 GROUP BY wine_type"""
-print("\nFirst query: average grade for each wine variety.")
+cursor = connection.cursor()
 cursor.execute(query1)
+attributes=cursor.description
+x1=[]
+y1=[]
 for rec in  cursor.fetchall():
-    print(rec[0], rec[1])
+    x1.append(rec[0])
+    y1.append(rec[1])
+fig = go.Figure([go.Bar(x=x1, y=y1)])
+py.plot(fig, auto_open=True, filename='graph1')
 
-
-query2 = """select count(province)/(select count(province)from Province)*100 as Procent, province from Province group by province"""
 print("\nSecond query: percent wineries in each province.")
-cursor.execute(query2, provincee_id = 'California')
+query2 = """select count(province)/(select count(province)from Province)*100 as Procent, province from Province group by province"""
+cursor.execute(query2)
+x2=[]
+y2=[]
+for rec1 in cursor.fetchall():
+    x2.append(rec1[0])
+    y2.append(rec1[1])
+
+colors = ['gold', 'mediumturquoise', 'lightgreen', 'cyan', '#E377C2']
+fig2 = go.Figure(data=[go.Pie(labels=y2,
+                             values=x2)])
+fig2.update_traces(marker=dict(colors=colors))
+py.plot(fig2, auto_open=True, filename='graph2')
 
 
-print(cursor.fetchone()[0], '%')
-
-
-query3 = """select * from CompanyScore;"""
 print("\nThird query: dependancy between winery and grade.")
+query3 = """select * from CompanyScore"""
 cursor.execute(query3)
+x3=[]
+y3=[]
+for rec in cursor.fetchall():
+    x3.append(rec[0])
+    y3.append(rec[1])
 
-for rec in  cursor.fetchall():
-    print(rec[0], rec[1])
-
+fig3= go.Figure(data=go.Scatter(x=x3, y=y3, mode='markers'))
+py.plot(fig3, auto_open=True, filename='graph3')
 cursor.close()
 
 connection.close()
