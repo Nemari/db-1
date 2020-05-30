4217308f09b23217588dcd57b68a0dad826bd778
Mariia Hrytsaienko <44789399+Nemari@users.noreply.github.com>
2020-05-30 18:37:27 +0300

    Create populate.sql

---
+++ laba4/populate.sql
@@ -0,0 +1,7 @@
+insert into customer(customer_id, customer_name) values (0, 'Shulyavka');
+insert into customer(customer_id, customer_name) values (1, 'Mafia');
+insert into customer(customer_id, customer_name) values (2, 'Mister Cat');
+
+insert into orders(order_id, wine_id, customer_id, amount) values (0, 2, 0, 1000);
+insert into orders(order_id, wine_id, customer_id, amount) values (1, 5, 1, 10);
+insert into orders(order_id, wine_id, customer_id, amount) values (2, 7, 2, 500);
